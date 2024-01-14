b777b4730d17cc0b944f2d038a7e3dded853cece
AdiLind3 <adi.lind@post.runi.ac.il>
2024-01-14 20:24:18 +0200

    a

--- StringOps.java
+++ StringOps.java
@@ -22,12 +22,50 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
+        String test = "One two tHRee world";
+        System.out.println(capVowelsLowRest(test));
         
     }
 
     public static String capVowelsLowRest (String string) {
         // Write your code here:
-        return "";
+        String result = ""; // create the string we want to get
+        String semiresult = lowerCase(string); // change the original string we got to without Upper-Case
+        int length = string.length();
+        for(int i = 0 ; i< length ; i++)
+        {
+            char temp = semiresult.charAt(i);
+            switch (temp) {
+                case 97: //a
+                    temp = (char) (temp - 32); //change a to A
+                    result = result + temp;
+                    break;
+            
+                default:
+                    result = result + temp;
+                    break;
+                case 101: //e
+                temp = (char) (temp - 32); //change e to E
+                    result = result + temp;
+                    break;
+
+                case 105: //i
+                temp = (char) (temp - 32); //change i to I
+                    result = result + temp;
+                    break;
+                case 111: //o
+                temp = (char) (temp - 32); //change o to O
+                    result = result + temp;
+                    break;
+
+                case 117: //u
+                temp = (char) (temp - 32); //change u to U
+                    result = result + temp;
+                    break;
+            }
+        }
+
+        return result;
     }
 
     public static String camelCase (String string) {
@@ -38,5 +76,26 @@ public class StringOps {
     public static int[] allIndexOf (String string, char chr) {
         // Write your code here:
         return new int[1];
+    }
+
+    public static String lowerCase(String s) { //from the HW3
+        int size = s.length();
+        String newS = ""; //make the new s we get but without Upper-case.
+        for(int i = 0 ; i < size; i++)
+        {
+            char temp = s.charAt(i);
+            if (temp >= 65 && temp <= 90) // the ascii code of upper
+            {
+                temp = (char) (temp + 32) ;
+                newS = newS + temp;
+            }
+            else 
+            {
+                newS = newS + temp;
+            }
+            
+        }
+
+        return newS;
     }
 }
